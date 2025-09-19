c7f52f1c47c7ae24b2516486595c604a36a6918e
Flavio MAC <alexandre.flavio658@gmail.com>
2025-09-19 09:30:12 -0300

    feat: ajuste em variavel ambiente

--- src/controllers/upload.controller.ts
+++ src/controllers/upload.controller.ts
@@ -22,7 +22,7 @@ const s3Client = new S3Client({
   },
 });
 
-const BUCKET_NAME = process.env.AWS_S3_BUCKET_NAME!;
+const BUCKET_NAME = process.env.AWS_S3_BUCKET!;
 
 // Configuração do Multer para upload em memória
 const upload = multer({
